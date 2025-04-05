03a60825d358fabb43da108b4a941695396f251c
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-04-05 23:44:18 +0500

    added icons for better interactions

--- src/app/components/Tab/index.tsx
+++ src/app/components/Tab/index.tsx
@@ -19,16 +19,17 @@ import Summarizer from "../Summarizer"
 import QuizCreator from "../QuizCreator"
 import QuizChecker from "../QuizChecker"
 import ChatInterface from "../ChatInterface"
+import { BadgePlus, Layers2, MessageCircleDashed, ShieldCheck } from 'lucide-react';
 
 export function TabsComponent() {
   return (
     <div className="flex h-screen">
       <Tabs defaultValue="chat" className="flex flex-row">
         <TabsList className="flex justify-start flex-col w-40 h-auto border-r">
-          <TabsTrigger value="chat" className="w-full py-4">Chat</TabsTrigger>
-          <TabsTrigger value="summarizer" className="w-full py-4">Summarizer</TabsTrigger>
-          <TabsTrigger value="quiz-generator" className="w-full py-4">Quiz Creator</TabsTrigger>
-          <TabsTrigger value="quiz-checker" className="w-full py-4">Quiz Checker</TabsTrigger>
+          <TabsTrigger value="chat" className="flex justify-start w-full py-2 space-x-1"><MessageCircleDashed /><p>Chat</p></TabsTrigger>
+          <TabsTrigger value="summarizer" className="flex justify-start w-full py-2 space-x-1"><Layers2 /><p>Summarizer</p></TabsTrigger>
+          <TabsTrigger value="quiz-generator" className="flex justify-start w-full py-2 space-x-1"><BadgePlus /><p>Quiz Creator</p></TabsTrigger>
+          <TabsTrigger value="quiz-checker" className="flex justify-start w-full py-2 space-x-1"><ShieldCheck /><p>Quiz Checker</p></TabsTrigger>
         </TabsList>
         <div className="flex-1">
           <TabsContent value="chat">
